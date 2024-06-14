caa7eeb92296fd9423fa05463db8c5d5b9689298
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2024-06-15 01:23:26 +0530

    auth and middleware

--- medium_project/server/src/index.ts
+++ medium_project/server/src/index.ts
@@ -1,12 +1,35 @@
 import { Hono } from 'hono'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { PrismaClient } from '@prisma/client/extension'
+import { PrismaClient } from '@prisma/client/edge'
+import { verify, sign, decode } from 'hono/jwt'
+import { use } from 'hono/jsx'
+
 
 const app = new Hono<{
   Bindings : {
     DATABASE_URL : string
+    JWT_SECRET : string
+    email : string
   }
 }>()
+
+// Middleware logic
+app.use('/api/v1/blog/*', async (c, next) => {
+  
+  const headers = c.req.header("authorization") || "";
+  //extracting the token from headers
+  const token = headers.split(" ")[1]
+  //@ts-ignore
+  const response = await verify(token, c.env.JWT_SECRET)
+  if(response.id){
+    await next();
+  } else {
+    return c.json({
+      status : 403,
+      error: " unauthorized"
+    })
+  }
+})
 
 
 // const prisma = new PrismaClient({
@@ -27,18 +50,63 @@ app.post('/api/v1/signup', async (c) => {
   }).$extends(withAccelerate())
   
   const body = await c.req.json() 
-
-  await prisma.User.create({
+  //@ts-ignore
+  const user = await prisma.User.create({
       data:{
-        email : c.body.email,
         //@ts-ignore
-        password : c.body.password
+        email : body.email,
+        //@ts-ignore
+        password : body.password
+      }
+  })
+  console.log(user)
+  if(!user){
+    return c.json({
+        status : 404,
+        msg :" user not created"
+    })
+  }
+  
+  const token = await sign({id : user.id} , c.env.JWT_SECRET)
+  console.log(token)
+  if(!token){
+    return c.json({
+      msg : "TOken not created"
+    })
+  }
+
+  return c.json({
+    token
+  })
+})
+
+app.post('/api/v1/signin', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl : c.env.DATABASE_URL
+  }).$extends(withAccelerate())
+
+  const body = await c.req.json()
+
+  const user = await prisma.user.findUnique({
+    //@ts-ignore
+      where :{
+        email : body.email,
+        password : body.password
       }
   })
 
-})
+  if(!user){
+    return c.json({
+      msg : "User not found"
+    })
+  }
 
-app.post('/api/v1/signin', (c) => {
+  const token = sign({email : user.email}, c.env.JWT_SECRET)
+
+  return c.json({
+    msg :" Sigin successfull",
+    token
+  })
 
 })
 
